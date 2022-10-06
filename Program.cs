07d9807cf1c597051fea71085a2df34d3dccd3cf
Fronkys <113862604+Fronkys@users.noreply.github.com>
2022-10-06 23:34:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,112 +1,138 @@
-﻿Console.WriteLine("1.Сложить 2 числа");
-Console.WriteLine("2.Вычесть первое из второго");
-Console.WriteLine("3.Перемножить два числа");
-Console.WriteLine("4.Разделить первое на второе");
-Console.WriteLine("5.Возвести в степень N первое число");
-Console.WriteLine("6.Найти квадратный корень из числа");
-Console.WriteLine("7.Найти 1 процент от числа");
-Console.WriteLine("8.Найти факториал из числа");
-Console.WriteLine("9.Выйти из программы");
+﻿using System;
+using System.Security.Cryptography;
 
-string Povtor = "yes";
-
-do
+namespace Игра
 {
-Console.WriteLine("Введите число от 1 до 9");
-int ChisloAmount = Convert.ToInt32(Console.ReadLine());
+    internal class Program
+    {
+        public static int Delitelb { get; private set; }
 
-if (ChisloAmount == 1)
-{
+        static void Main()
+        {
+            Console.WriteLine("Для работы программы введите число программы из списка: ");
+            Console.WriteLine("1.Угадай число");
+            Console.WriteLine("2.Таблица умножения");
+            Console.WriteLine("3.Вывод делителей числа");
+            Console.WriteLine("4.Выйти из программы");
+            int Chislo = Convert.ToInt32(Console.ReadLine());
+            
+            if (Chislo == 1)
+            {
+                UgadaiChislo();
+            }
+            else if (Chislo == 2)
+            {
+                TablisaYmnozhenia();
+            }
+            else if (Chislo == 3)
+            {
+                VivodDeliteleiChisla();
+            }
+            else if (Chislo == 4)
+            {
+                Console.WriteLine("Программа завершена");
+            }
+            else
+                Console.WriteLine("Вы ввели неправильное число");
 
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-Double y = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Итог: ");
-Console.WriteLine(x + y);
+        }
+        static void UgadaiChislo()
+        {
 
+            Random rand = new Random();
+            int Pravchislo = rand.Next(100) + 1;
+
+            do
+            {
+                Console.WriteLine("Введите число: ");
+                int ChisloGuess = Convert.ToInt32(Console.ReadLine());
+                if (ChisloGuess == Pravchislo)
+                {
+                    Console.WriteLine("Успешно угадали");
+                    break;
+                }
+                else
+                {
+                    if (ChisloGuess > Pravchislo)
+                    {
+                        Console.WriteLine("Число меньше загаданного");
+
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Число больше загаданного");
+
+                    }
+                    
+                }
+
+
+            } while (true);
+            Main();
+        }
+
+    
+        static void TablisaYmnozhenia()
+        {
+            int[,] massiv = new int[10, 10];
+            for (int stroka = 1; stroka < massiv.GetLength(0); ++stroka)
+            {
+                for (int stolbiki = 1; stolbiki < massiv.GetLength(1); ++stolbiki)
+                {
+
+                    int sum = stroka * stolbiki;
+                    if (stroka == stolbiki)
+                    {
+                        massiv[stroka, stolbiki] = sum;
+                        Console.Write(massiv[stroka, stolbiki] + "\t");
+                    }
+                    else if (stroka > stolbiki)
+                    {
+                        massiv[stroka, stolbiki] = sum;
+                        Console.Write(massiv[stroka, stolbiki] + "\t");
+                    }
+                    else if (stroka < stolbiki)
+                    {
+                        massiv[stroka, stolbiki] = sum;
+                        Console.Write(massiv[stroka, stolbiki] + "\t");
+                    }
+                    
+
+                }
+                Console.WriteLine("\n\n");
+                
+            }
+            Main();
+        }
+
+        static void VivodDeliteleiChisla()
+        {
+
+            string Povtor = "yes";
+
+            do
+            {
+                Console.Write("Введите число: ");
+                int delimoe = Convert.ToInt32(Console.ReadLine());
+                for (Delitelb = 1; Delitelb <= delimoe; Delitelb++)
+                {
+                    if (delimoe % Delitelb == 0)
+                    {
+                        Console.WriteLine(Delitelb + "\n");
+                    }
+                }
+                Console.WriteLine("Хотите продолжить? (yes/no)");
+                Povtor = Convert.ToString(Console.ReadLine());
+
+            } while (Povtor == "yes");
+
+            
+            Main();
+            
+        } 
+       
+
+    }
 }
-
-else if (ChisloAmount == 2)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-Double y = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Итог: ");
-Console.WriteLine(x - y);
-
-}
-else if (ChisloAmount == 3)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-Double y = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Итог: " + x * y);
-
-
-}
-else if (ChisloAmount == 4)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-Double y = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Итог: " + x / y);
-
-
-}
-else if (ChisloAmount == 5)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Введите степень, в которую хотите возвести число : ");
-Double y = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Итог: " + Math.Pow(x, y));
-
-
-}
-else if (ChisloAmount == 6)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Итог: " + Math.Sqrt(x));
-
-
-}
-else if (ChisloAmount == 7)
-{
-Console.WriteLine("Введите первое число: ");
-Double x = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Итог 1% = " + x / 100);
-
-}
-else if (ChisloAmount == 8)
-{
-Console.WriteLine("Введите число, которое хотите возвести в факториал: ");
-int x = Convert.ToInt32(Console.ReadLine());
-int f = 1;
-for (int i = 1; x >= i; ++i)
-{
-f = f * i;
-}
-Console.WriteLine(f);
-}
-else if (ChisloAmount == 9)
-{
-Console.WriteLine("Программа завершена");
-break;
-}
-Console.WriteLine("Хотите продолжить? (yes/no)");
-Povtor = Convert.ToString(Console.ReadLine());
-} while (Povtor == "yes");
-
